0fabb58643975d3ff5f479938cdac0004471b144
elsagheer <ahmedelsogheer@paymob.com>
2023-02-10 03:01:48 +0200

    finish week3 hw

--- week_3_data_warehouse/big_query_hw.sql
+++ week_3_data_warehouse/big_query_hw.sql
@@ -1,30 +1,54 @@
-CREATE OR REPLACE EXTERNAL TABLE `taxi-rides-ny.nytaxi.fhv_tripdata`
+CREATE OR REPLACE EXTERNAL TABLE `dezoomcamp.external_fhv_tripdata`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://nyc-tl-data/trip data/fhv_tripdata_2019-*.csv']
+  uris = ['gs://prefect-de-zoomcamp-lol/data/raw/fhv/fhv*.csv.gz']
 );
 
+CREATE OR REPLACE TABLE `dezoomcamp.fhv_tripdata` as
+select * from dezoomcamp.external_fhv_tripdata ;
+-- Q1
+SELECT count(*) FROM `dezoomcamp.fhv_tripdata`;
+-- 43,244,696
+----------------------
+-- Q2
+SELECT COUNT(DISTINCT(affiliated_base_number)) FROM `dezoomcamp.external_fhv_tripdata`;
+-- 0 B
+SELECT COUNT(DISTINCT(affiliated_base_number)) FROM `dezoomcamp.fhv_tripdata`;
+-- 317.94MB
+----------------
+-- Q3
+select count(*) FROM `dezoomcamp.fhv_tripdata` 
+where PUlocationID is null  and  DOlocationID is null ; 
+-- 717,748
+---------------------------
+-- Q4 
+-- Question 4: What is the best strategy to make an optimized table in Big Query if your query will always filter by pickup_datetime and order by affiliated_base_number?
+-- Partition by pickup_datetime Cluster on affiliated_base_number
+------------------------------
+-- Q5
+CREATE OR REPLACE TABLE
+  dezoomcamp.fhv_tripdata_partioned_clustered
+PARTITION BY
+  DATE(pickup_datetime)
+CLUSTER BY
+  affiliated_base_number AS
+SELECT
+  *
+FROM
+  dezoomcamp.fhv_tripdata;
 
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
+  select distinct(affiliated_base_number) from   dezoomcamp.fhv_tripdata
+  where   DATE(pickup_datetime) >='2019-03-01' and  DATE(pickup_datetime) <='2019-03-31' ;
+-- 647.87 MB 
 
-
-SELECT COUNT(DISTINCT(dispatching_base_num)) FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
-
-
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-AS SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`;
-
-CREATE OR REPLACE TABLE `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-PARTITION BY DATE(dropoff_datetime)
-CLUSTER BY dispatching_base_num AS (
-  SELECT * FROM `taxi-rides-ny.nytaxi.fhv_tripdata`
-);
-
-SELECT count(*) FROM  `taxi-rides-ny.nytaxi.fhv_nonpartitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
-
-
-SELECT count(*) FROM `taxi-rides-ny.nytaxi.fhv_partitioned_tripdata`
-WHERE dropoff_datetime BETWEEN '2019-01-01' AND '2019-03-31'
-  AND dispatching_base_num IN ('B00987', 'B02279', 'B02060');
+  select distinct(affiliated_base_number) from   dezoomcamp.fhv_tripdata_partioned_clustered
+  where   DATE(pickup_datetime) >='2019-03-01' and  DATE(pickup_datetime) <='2019-03-31' ;
+--  23.06 MB
+------------------------------
+-- Q6
+-- Question 6: Where is the data stored in the External Table you created?
+-- GCP Bucket
+--------------------------------------------------------------
+-- Q7
+-- Question 7: It is best practice in Big Query to always cluster your data.
+-- False
